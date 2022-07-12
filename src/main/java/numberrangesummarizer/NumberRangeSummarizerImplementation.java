e9e3538cb5dac065df176bb761196c3e670b4754
Bheki Ndhlovu <bhekindhlovu7@gmail.com>
2022-07-12 13:24:49 +0200

    adding test names,descriptions and adding final tests

--- src/main/java/numberrangesummarizer/NumberRangeSummarizerImplementation.java
+++ src/main/java/numberrangesummarizer/NumberRangeSummarizerImplementation.java
@@ -1,29 +1,33 @@
 package numberrangesummarizer;
+
 import java.util.*;
 import java.util.stream.Collectors;
+
 import static java.util.stream.Collectors.toCollection;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 /**
  * @author Bheki Ndhlovu
- *
+ * <p>
  * This class will implement the two methods from the NumberRangeSummerizer interface.
  */
 public class NumberRangeSummarizerImplementation
-        implements NumberRangeSummarizer{
-    private static final Pattern pattern = Pattern.compile("[0-9]+(,[0-9]+)+");
+        implements NumberRangeSummarizer {
+
+    private static final Pattern pattern = Pattern.compile("-?[0-9]+(,-?[0-9]+)+");
     private Matcher matcher;
 
     /**
      * @author Bheki Ndhlovu
-     *
+     * <p>
      * This method will accept a comma delineated string of integer.
      * It will then process the string into a collection and return a collection of integers.
      */
     @Override
     public Collection<Integer> collect(String input) {
-        if(input!=null&&pattern.matcher(input).matches())
-        {
+        if (input != null && pattern.matcher(input).matches()) {
             return Arrays.stream(input.split(","))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
@@ -33,28 +37,25 @@ public class NumberRangeSummarizerImplementation
 
     /**
      * @author Bheki Ndhlovu
-     *
+     * <p>
      * This method will accept a collection of integers.
      * It will then sort the array and collapse sequential numbers into ranges.
      * It will finally parse the array into a string and re
      */
     public String summarizeCollection(Collection<Integer> input) {
-        Integer[] arr = input.stream().sorted().collect(toCollection(ArrayList::new)).toArray(new Integer[0]);
-        Integer end,start;
+        Integer[] arr = input.stream().sorted().distinct().collect(toCollection(ArrayList::new)).toArray(new Integer[0]);
+        Integer end, start;
         end = start = arr[0];
         StringBuilder result = new StringBuilder("");
-        for (int i = 1; i < arr.length; i++)
-        {
-            if (arr[i].equals(arr[i - 1] + 1))
-            {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i].equals(arr[i - 1] + 1)) {
                 end = arr[i];
-            }
-            else{
-                if(start.equals(end)) {
+            } else {
+                if (start.equals(end)) {
                     result.append(start).append(",");
-                }else if (end.equals(start + 1)){
+                } else if (end.equals(start + 1)) {
                     result.append(start).append(",").append(end).append(",");
-                }else {
+                } else {
                     result.append(start).append("-").append(end).append(",");
                 }
                 start = end = arr[i];
@@ -62,8 +63,7 @@ public class NumberRangeSummarizerImplementation
         }
         if (start.equals(end)) {
             result.append(start);
-        }
-        else{
+        } else {
             result.append(start).append("-").append(end);
         }
         return result.toString();
--- src/test/java/NumberRangeSummerizerTest.java
+++ src/test/java/NumberRangeSummerizerTest.java
@@ -1,8 +1,11 @@
+import com.sun.org.glassfish.gmbal.Description;
 import numberrangesummarizer.NumberRangeSummarizerImplementation;
 import java.util.Arrays;
 import java.util.Collection;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
 import static junit.framework.TestCase.assertEquals;
 
 /**
@@ -13,11 +16,15 @@ import static junit.framework.TestCase.assertEquals;
 public class NumberRangeSummerizerTest {
     private NumberRangeSummarizerImplementation numberRangeSummarizer = new NumberRangeSummarizerImplementation();
 
+    @DisplayName("Check Collection Size")
+    @Description("This test will ensure that the correct collection size is returned based on the number of integers in the string")
     @Test
     public void correctCollectionSize() {
        assertEquals(numberRangeSummarizer.collect("1,2,3").size(), 3);
     }
 
+    @DisplayName("Check collection contains integers")
+    @Description("This test will ensure that the collection is not empty and actually contains integers")
     @Test
     public void numberArrayIsNotEmpty() {
         Collection<Integer> numbers = numberRangeSummarizer.collect("1,3,6,7,8,12,13,14,15,21,22,23,24,31");
@@ -25,12 +32,26 @@ public class NumberRangeSummerizerTest {
         Assert.assertEquals(numbers.iterator().hasNext(), true);
     }
 
+    @DisplayName("Check collection is sequential")
+    @Description("This test will ensure that the collection is sequential")
     @Test
     public void collectionIsSequential() {
         Collection<Integer> numbers = numberRangeSummarizer.collect("1,3,6,7,8,12,13,14,15,21,22,23,24,31");
         Assert.assertFalse(Boolean.parseBoolean(Arrays.stream(numbers.toArray()).unordered().toString()));
     }
 
+    @DisplayName("Check that strings with duplicates are summarized")
+    @Description("This test will ensure that a normal string of comma delimited of numbers in ascending order is summarized")
+    @Test
+    public void normalInputWithDuplicates() {
+        String inputNumbers = "1,3,3,3,3,3,3,3,3,3,3,6,6,6,6,7,8,12,13,14,14,14,15,21,21,21,22,22,22,22,22,22,23,23,23,23,23,23,24,31";
+        String expectedSummary = "1,3,6-8,12-15,21-24,31";
+        Collection<Integer> numbers = numberRangeSummarizer.collect(inputNumbers);
+        Assert.assertEquals(numberRangeSummarizer.summarizeCollection(numbers), expectedSummary);
+    }
+
+    @DisplayName("Check normal sequential numbers are summarized")
+    @Description("This test will ensure that a normal string of comma delimited of numbers in ascending order is summarized")
     @Test
     public void normalSequentialNumbersAreSummarized() {
         String inputNumbers = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
@@ -39,6 +60,8 @@ public class NumberRangeSummerizerTest {
         Assert.assertEquals(numberRangeSummarizer.summarizeCollection(numbers), expectedSummary);
     }
 
+    @DisplayName("Check non sequential strings are summarized")
+    @Description("This test will ensure that strings with no sequential numbers inputted will return the same string")
     @Test
     public void nonSequentialNumberStringsAreSummarized() {
         String inputNumbers = "1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31";
@@ -47,6 +70,22 @@ public class NumberRangeSummerizerTest {
         Assert.assertEquals(numberRangeSummarizer.summarizeCollection(numbers), expectedSummary);
     }
 
+    @DisplayName("Check that Negative Strings are summarized")
+    @Description("This test will ensure that strings with negative numbers are summarized")
+    @Test
+    public void negativeSequentialNumbersAreSummarized() {
+        StringBuilder sb = new StringBuilder();
+        String expectedSummary = "-10-0";
+        for (int i = -10; i <= 0; i++) {
+            sb.append(i);
+            sb.append(",");
+        }
+        sb.setLength(sb.length() - 1);
+        Assert.assertEquals(numberRangeSummarizer.summarizeCollection(numberRangeSummarizer.collect(sb.toString())),expectedSummary);
+    }
+
+    @DisplayName("Check that large sequential strings are summarized")
+    @Description("This test will ensure that large strings will be summarized efficiently")
     @Test
     public void largerSequentialStringsAreSummarized() {
         StringBuilder sb = new StringBuilder();
@@ -59,6 +98,8 @@ public class NumberRangeSummerizerTest {
         Assert.assertEquals(numberRangeSummarizer.summarizeCollection(numberRangeSummarizer.collect(sb.toString())),expectedSummary);
     }
 
+    @DisplayName("Check that short strings are summarized")
+    @Description("This test will ensure that short strings comprising of two elements are summarized")
     @Test
     public void shortSequentialStringsAreSummarized() {
         String input = "1,2";
@@ -66,16 +107,22 @@ public class NumberRangeSummerizerTest {
         Assert.assertEquals(numberRangeSummarizer.summarizeCollection(numberRangeSummarizer.collect(input)),expectedSummary);
     }
 
+    @DisplayName("Check that IllegalArgumentException is thrown for empty string")
+    @Description("This test will ensure that is a empty string is collected the correct exception will be thrown")
     @Test(expected=IllegalArgumentException.class)
     public void collectEmptyString() throws IllegalArgumentException{
         Collection<Integer> actual = numberRangeSummarizer.collect("");
     }
 
+    @DisplayName("Check that special characters in string throw exception")
+    @Description("This test will ensure that strings containing special characters will throw exception")
     @Test(expected=IllegalArgumentException.class)
     public void incorrectFormat() throws IllegalArgumentException{
-        Collection<Integer> actual = numberRangeSummarizer.collect("1234567");
+        Collection<Integer> actual = numberRangeSummarizer.collect("1,2,3,$,6");
     }
 
+    @DisplayName("Check that IllegalArgumentException is thrown for null input")
+    @Description("This test will ensure that a null pointer exception is thrown for any ")
     @Test(expected=IllegalArgumentException.class)
     public void nullInput() throws IllegalArgumentException {
         Collection<Integer> actual = numberRangeSummarizer.collect(null);
